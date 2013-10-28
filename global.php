ffc4d4657d160bfc54d614340dab993e15489b51
vitalyiegorov <egorov@samsonos.com>
2013-10-28 10:01:59 +0200

    init

---
+++ global.php
@@ -0,0 +1,41 @@
+<?php
+use Samson\i18n\i18n;
+/**
+ * Translate(Перевести) фразу
+ *
+ * @param string $key 		Ключ для поиска перевода фразы
+ * @param string $locale 	Локаль в которую необходимо перевести
+ * @return string Переведенная строка или просто значение ключа
+ */
+function t( $key, $return = false, $locale = NULL )
+{
+	// т.к. эта функция вызывается очень часто - создадим статическую переменную
+	static $_v;
+
+	// Если переменная не определена - получим единственный экземпляр ядра
+	if( !isset($_v)) $_v = & m('i18n');	
+
+	// Вернем указатель на ядро системы
+	if (!$return)echo $_v->translate( $key, $locale );
+	else return $_v->translate( $key, $locale );
+}
+
+/**
+ * Generate corrert plural word form from dictionary
+ * @param string $key 	Dictionary key
+ * @param string $count Amount for plural form
+ * @return string Word in correct plural form
+ */
+function plural( $key, $count )
+{	
+	$dict = & $GLOBALS['dict_plural'][ $key ];
+	if( isset($dict)) switch($count % 20)
+	{
+		case 1: return $dict[0]; break;
+		case 2:
+		case 3:
+		case 4:	return $dict[1]; break;
+		default: return $dict[2]; break;
+	}
+	else return $key;	
+}
---
+++ i18n.php
@@ -0,0 +1,111 @@
+<?php
+namespace samson\i18n;
+
+use samson\core\CompressableExternalModule;
+
+/** Стандартный путь к папке со словарями */
+if(!defined('__SAMSON_I18N_PATH'))  define('__SAMSON_I18N_PATH', __SAMSON_APP_PATH.'/i18n' );
+
+/** Стандартный путь главному словарю сайта */
+if(!defined('__SAMSON_I18N_DICT'))  define('__SAMSON_I18N_DICT', __SAMSON_I18N_PATH.'/dictionary.php' );
+
+/**
+ * Интернализация / Локализация
+ *
+ * @author Vitaly Iegorov <vitalyiegorov@gmail.com>
+ * @version 1.0
+ */
+class i18n extends CompressableExternalModule
+{	
+	/** Идентификатор модуля */
+	public $id = 'i18n';
+	
+	/** Автор модуля */
+	public $author = 'Vitaly Iegorov';	
+	
+	/** Текущая локаль */
+	public $locale = 'en';
+	
+	/** Путь к файлу словаря */
+	public $path;
+	
+	/** Коллекция данных для перевода */
+	public $dictionary = array( 'ru' => array() );
+	
+	/** @see \samson\core\ModuleConnector::prepare() */
+	/*public function prepare()
+	{
+		
+	}*/
+	
+	/** Конструктор */
+	//public function __construct()
+	//{		
+		// Если существует главный словарь 
+		//if( file_exists( __SAMSON_I18N_DICT ) ) 
+		//{			
+			// Загрузим его содержимое
+			//$data = include(__SAMSON_I18N_DICT);	
+			//echo '!!!!!!!!!!!!!!!!! - __construct - !!!!!!!!!!!!!!';
+			//include(__SAMSON_I18N_DICT);
+			/*$data = \dictionary ();
+			// Пробежимся по локалям в словаре
+			foreach ( $data as $locale => $dict )
+			{			
+				// Создадим словарь для локали
+				$this->data[ $locale ] = array();
+				
+				// Преобразуем ключи 
+				foreach ( $dict as $k => $v ) $this->data[ $locale ][ (trim($k)) ] = $v;
+			}	*/	 
+		//}
+	//}
+	
+	public function init( array $params = array() )
+	{
+		parent::init();
+		$this->create();
+	}
+	
+	public function create()
+	{
+		if( file_exists( __SAMSON_I18N_DICT ) )
+		{
+			include(__SAMSON_I18N_DICT);
+		}
+		$data = \dictionary ();
+		// Пробежимся по локалям в словаре
+		foreach ( $data as $locale => $dict )
+		{
+			// Создадим словарь для локали
+			$this->data[ $locale ] = array();
+		
+			// Преобразуем ключи
+			foreach ( $dict as $k => $v ) $this->data[ $locale ][ (trim($k)) ] = $v;
+		}
+	}
+	
+	/**
+	 * Translate(Перевести) фразу
+	 *
+	 * @param string $key 		Ключ для поиска перевода фразы
+	 * @param string $locale 	Локаль в которую необходимо перевести
+	 * @return string Переведенная строка или просто значение ключа
+	 */
+	function translate( $key, $locale = NULL )
+	{
+		// Если требуемая локаль не передана - получим текущую локаль
+		if( !isset( $locale ) ) $locale = locale();
+		
+		// Получим словарь для нужной локали
+		$dict = & $this->data[ $locale ];
+		
+		// Получим хеш строки
+		$md5_key = (trim( $key ));
+		
+		// Попытаемся найти запись в словаре
+		if( isset( $dict[ $md5_key ] ) ) return $dict[ $md5_key ];
+		// Просто вернем ключ		
+		else return $key;
+	}
+}
